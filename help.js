a547922f3fcb4864055316fd6d3aa9535b515b2b
madloyer <87596107+madloyer@users.noreply.github.com>
2022-12-04 20:35:35 +0700

    Update help.js

--- help.js
+++ help.js
@@ -1,20 +1,46 @@
-module.exports.bothelp = `Yoo, Guwe botnya Masakini Channel.`;
+module.exports.bothelp = `Yoo, Guwe botnya Mantapjozz Channel. Untuk Join Group Vip PPV silahkan cek list dibawah.`;
 
 module.exports.botcommand = `<u>Berikut adalah beberapa perintah dan penggunaan admin.</u>
 \n<b>Kegunaan Nomor US untuk agar dapat mengakses Group/Channel Yang di banned dari Nomor indo.</b>
 <b>Untuk pembelian bisa langsung Chat admin dibawah va Wa :</b>
-admin 1 
-admin 2 
+admin 1 : 
+admin 2 : 
 \nKalau gak mau beli atau mau gratisan bisa juga langsung bikin sendiri dengan gunain APK 2NDLINE\nBisa download di Playstore ataupun Googling aja dan untuk Tutorial bisa cek YT admin dibawah:
 <a href="https://youtu.be/920nA1E8ON8">Tutorial Bikin Nomor US (2ndline)</a>
 Ikuti tutorialnya yang benar biar bisa langsung dapet dan jangan nyerah jika gagal #tetapcoliwalaugakberguna
 \nUntuk Contoh testi atau contoh group yang ke banned nomor indo sehingga butuh akses Nomor Us bisa kalian kepo di group dibawah ini:
+
 `;
 
 module.exports.botinstall = `<u>List Join Group Vip PPV / Full Genre Indo, asian, West.</u>
-
+Join Vip Gudang Bokep
+Group Vip Permanent
+==================
+Group Full Indo : 50k
+Group Full Asian : 50k
+Group Full West : 50k
+Group Vip PPV  : 65k
+\nNomor Luar / US : 45k
+==================
+Group Full Indo, Asian , West : 200K
+Group VIP PPV +  Nomor US = 100K
+==================
+Group Full + PPV + Nomor Luar Negeri = 250K
+==================
+info : untuk Group PPV Media 250k ++ dan butuh Nomor selain +62 untuk akses.
+================
+note : 
+- pembayaran via ovo, dana, gopay 
+================
+untuk join silahkan chat admin👇
+admin : 
 `;
 
 module.exports.botorder = `<u>Suka sama BOT gini ? mau punya yang sama gini ?</u>
-\n
+\nKuy langsung Chat admin untuk Order ataupun untuk source Gratisnya.
+================
+Untuk Source Gratisnya Bisa Langsung Visit Link Ini : <a href="https://github.com/softmilkpc/MantapjozzSaverGroup">Softmilk</a>
+Kalau Mau tinggal beres langsung chat admin.
+================
+Order Bot silahkan chat admin👇 .
 `;
